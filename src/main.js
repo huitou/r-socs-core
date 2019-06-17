0fe9c2c0b7415a8849a6b3dd51294c3df30673ba
huitou <3.riverside.swe@gmail.com>
2019-06-17 05:51:20 +0100

    adding copyright

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-// src/main.js
+/*
+    Library main.
+
+    Copyright (c) 2018 Riverside Software Engineering Ltd. All rights reserved.
+
+    Licensed under the MIT License. See LICENSE file in the project root for full license information.
+*/
+
 import Collector from './Collector';
 import { withCollector } from './withCollector';
 import { connect } from './connect';
