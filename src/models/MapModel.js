8b3ca756fc5de4a26d0ecfb7b0c746cabf912e67
huitou <3.riverside.swe@gmail.com>
2019-08-31 13:35:52 +0100

    removing hprops

--- src/models/MapModel.js
+++ src/models/MapModel.js
@@ -6,21 +6,20 @@
 	Licensed under the MIT License. See LICENSE file in the project root for full license information.
 */
 import React, { Component, Fragment } from 'react';
-import { any, objectOf, elementType } from 'prop-types';
+import { objectOf, elementType } from 'prop-types';
 
 import Collector, { withCollector } from '../collector/index';
 
 class MapModelComponent extends Component {
 	static propTypes = {
-		hprops: any,
 		map: objectOf(elementType).isRequired,
 	};
 
 	mappedNodes = () => {
-		const { hprops, map } = this.props;
+		const { map, hset, ...rest } = this.props;
 
 		return Object.entries(map).map(([name, Model]) => {
-			return (<Model key={name} {...hprops} hset={this.hset(name)} />);
+			return (<Model key={name} {...rest} hset={this.hset(name)} />);
 		});
 	};
 
