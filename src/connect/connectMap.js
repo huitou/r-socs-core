105d839177475384e7a3ea02dedea6e31c132c5c
huitou <3.riverside.swe@gmail.com>
2020-04-10 20:18:00 +0100

    n.16 - updating copyright for connect

--- src/connect/connectMap.js
+++ src/connect/connectMap.js
@@ -1,7 +1,7 @@
 /*
 	connectMap - It connects a map of model components to a target component and inject provided values and handles.
 
-	Copyright (C) 2019 Riverside Software Engineering Ltd. All rights reserved.
+	Copyright (C) 2019-2020 Riverside Software Engineering Ltd. All rights reserved.
 
 	Licensed under the MIT License. See LICENSE file in the project root for full license information.
 */
